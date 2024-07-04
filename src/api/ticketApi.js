c8bcfc9742ef0f000ca9f815d527f2bf34ab3bfa
manan2002sharma <109142063+manan2002sharma@users.noreply.github.com>
2024-07-04 19:30:34 +0530

    first commit

---
+++ src/api/ticketApi.js
@@ -0,0 +1,94 @@
+import axios from "axios";
+
+const rootUrl = "https://crm-backend-act7.onrender.com/v1/";
+const ticketUlr = rootUrl + "ticket/";
+const closeTicketUrl = rootUrl + "ticket/close-ticket/";
+
+export const getAllTickets = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios.get("https://crm-backend-act7.onrender.com/v1/ticket", {
+        headers: {
+          Authorization: sessionStorage.getItem("accessJWT"),
+        },
+      });
+
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+export const getSingleTicket = (_id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios.get(ticketUlr + _id, {
+        headers: {
+          Authorization: sessionStorage.getItem("accessJWT"),
+        },
+      });
+
+      resolve(result);
+    } catch (error) {
+      console.log(error.message);
+      reject(error);
+    }
+  });
+};
+
+export const updateReplyTicket = (_id, msgObj) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios.put(ticketUlr + _id, msgObj, {
+        headers: {
+          Authorization: sessionStorage.getItem("accessJWT"),
+        },
+      });
+
+      resolve(result.data);
+    } catch (error) {
+      console.log(error.message);
+      reject(error);
+    }
+  });
+};
+
+export const updateTicketStatusClosed = (_id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios.patch(
+        closeTicketUrl + _id,
+        {},
+        {
+          headers: {
+            Authorization: sessionStorage.getItem("accessJWT"),
+          },
+        }
+      );
+
+      resolve(result.data);
+    } catch (error) {
+      console.log(error.message);
+      reject(error);
+    }
+  });
+};
+
+export const createNewTicket = (frmData) => {
+  console.log("from api", frmData);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios.post(ticketUlr, frmData, {
+        headers: {
+          Authorization: sessionStorage.getItem("accessJWT"),
+        },
+      });
+
+      resolve(result.data);
+    } catch (error) {
+      console.log(error.message);
+      reject(error);
+    }
+  });
+};
